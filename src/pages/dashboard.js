a91eef38aee9320ad064769129d3ca949a126bed
alinojomi <anojomi3@gmail.com>
2023-07-06 10:21:04 +0330

    Added Dasboard Banner, added time and date clock to it and amde it responsive

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,14 +1,40 @@
 import SideNavBar from "../components/sideNavBar";
 import style from "../styles/dashboard.module.css";
+import {useState, useEffect} from "react";
 
 export default function Dashboard(props) {
     const shiftContent = true;
+
+    const [date, setDate] = useState({
+        hourTime: 0, minTime: 0, secTime: 0, yearTime: 0, monTime: 0, dayTime: 0,
+    });
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const timeDate = new Date();
+            setDate({
+                hourTime: timeDate.getHours().toString().padStart(2, '0'),
+                minTime: timeDate.getMinutes().toString().padStart(2, '0'),
+                secTime: timeDate.getSeconds().toString().padStart(2, '0'),
+                yearTime: timeDate.getFullYear().toString().padStart(2, '0'),
+                monTime: (timeDate.getMonth() + 1).toString().padStart(2, '0'),
+                dayTime: timeDate.getMonth().toString().padStart(2, '0'),
+            });
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
     return (<section className={style["dashboard-container"]}>
         <SideNavBar></SideNavBar>
         <div
             className={shiftContent ? `${style["content-container"]} ${style["shift"]}` : `${style["content-container"]}`}>
-            <p>hello world</p>
+            <div className={style["dashboard-banner"]}>
+                <div className={style["time-box"]}>{date.hourTime} : {date.minTime} : {date.secTime}</div>
+                <div className={style["date-box"]}>{date.yearTime} / {date.monTime} / {date.dayTime}</div>
+            </div>
         </div>
     </section>);
 }
