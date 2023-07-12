74d3ac79327e7894ea073aa7cfc071b5bcb8e852
shahab bahojb <bahojb.sb@gmail.com>
2023-07-12 13:44:54 +0330

    fix slider

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,6 +1,16 @@
 import { useState, useEffect } from "react";
 import style from "../styles/dashboard.module.css";
 import PieChart from "./charts/pieChart";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import image1 from "../image/image1.webp";
+import image2 from "../image/image2.jpg";
 
 export default function Dashboard(props) {
   const shiftContent = true;
@@ -34,7 +44,13 @@ export default function Dashboard(props) {
 
   return (
     <>
-      <div className={shiftContent ? `${style["content-container"]} ${style["shift"]}` : `${style["content-container"]}`}>
+      <div
+        className={
+          shiftContent
+            ? `${style["content-container"]} ${style["shift"]}`
+            : `${style["content-container"]}`
+        }
+      >
         <div className={style["dashboard-banner"]}>
           <div className={style["time-box"]}>
             {date.hourTime} : {date.minTime} : {date.secTime}
@@ -48,8 +64,29 @@ export default function Dashboard(props) {
             <PieChart></PieChart>
           </div>
           <div className={style["data-conatiner"]}>
-            <div className={style["swiper-container"]}>hello</div>
-            <div className={style["data-box"]}>hello</div>
+            <div className={style["swiper-container"]}>
+              {" "}
+              <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log("slide change")}
+              >
+                <SwiperSlide>
+                  {" "}
+                  <img src={image1} alt="image1" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  {" "}
+                  <img src={image2} alt="image1" />
+                </SwiperSlide>
+              </Swiper>
+            </div>
           </div>
         </div>
       </div>
