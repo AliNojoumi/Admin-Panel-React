1e4f7add5902d0677f60bd87579f8e52ff8b2ad2
alinojomi <anojomi3@gmail.com>
2023-07-20 10:19:04 +0330

    Removed dataTable

--- src/pages/users.js
+++ src/pages/users.js
@@ -1,10 +1,5 @@
 import style from "../styles/users.module.css";
-import DataTable from "../components/dataTable/dataTable";
 
 export default function Users(props) {
-  return (
-    <section className={style["users-container"]}>
-      <DataTable></DataTable>
-    </section>
-  );
+  return <section className={style["users-container"]}>hello</section>;
 }
