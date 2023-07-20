a29c22b79cf7bffd28efe9ec60ca2e498f9ade04
alinojomi <anojomi3@gmail.com>
2023-07-20 09:17:57 +0330

    Added stakedChart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./pages/main";
 import Dashboard from "./components/dashboard";
 import AddUser from "./pages/addUser";
 import Error from "./pages/error";
+import Chart from "./pages/chart";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" exact element={<Main />}>
             <Route path="/Dashboard" element={<Dashboard />}></Route>
             <Route path="/AddUsers" element={<AddUser />}></Route>
+            <Route path="/Charts" element={<Chart />}></Route>
           </Route>
           <Route path="*" element={<Error></Error>}></Route>
         </Routes>
---
+++ src/pages/chart.js
@@ -0,0 +1,3 @@
+export default function Chart() {
+  return <section>hello</section>;
+}
