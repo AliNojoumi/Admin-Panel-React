932a00531d33f8fcd1e1d16c9af49429ccee7f60
alinojomi <anojomi3@gmail.com>
2023-07-23 09:45:56 +0330

    Worked on usersList in usersData with css file and made it responsive

--- src/components/usersData.js
+++ src/components/usersData.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { TbLoader } from "react-icons/tb";
+import { TbLoader, TbEdit, TbArchive } from "react-icons/tb";
 import style from "../styles/usersData.module.css";
 
 export default function UsersData(props) {
@@ -37,7 +37,24 @@ export default function UsersData(props) {
           {fetchedData.map((item) => {
             return (
               <li key={item.id} className={style["item-li"]}>
-                {item.name}
+                <div className={style["item-container"]}>
+                  <p className={style["item-p"]}>
+                    Name : <span className={style["span"]}>{item.name}</span>
+                  </p>
+                  <p className={style["item-p"]}>
+                    SurName : <span className={style["span"]}>{item.sureName}</span>
+                  </p>
+                  <p className={style["item-p"]}>
+                    Age : <span className={style["span"]}>{item.age}</span>
+                  </p>
+                  <p className={style["item-p"]}>
+                    City : <span className={style["span"]}>{item.city}</span>
+                  </p>
+                  <p className={`${style["item-p"]} ${style["item-message"]}`}>
+                    Message : <span className={style["span"]}>{item.message}</span>
+                  </p>
+                </div>
+                <div className={style["icon-container"]}>icons</div>
               </li>
             );
           })}
