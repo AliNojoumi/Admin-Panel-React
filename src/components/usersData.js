1df3941c46fb173149ef165919a52fe39f992a51
alinojomi <anojomi3@gmail.com>
2023-07-23 19:30:08 +0330

    Added delete fetch api to the usersData with css file, made it responsive. didn't add success or error handling to it

--- src/components/usersData.js
+++ src/components/usersData.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { TbLoader, TbEdit, TbArchive } from "react-icons/tb";
+import { TbLoader, TbEdit, TbArchive, TbAlertCircle } from "react-icons/tb";
 import style from "../styles/usersData.module.css";
 
 export default function UsersData(props) {
@@ -26,6 +26,22 @@ export default function UsersData(props) {
     }
   }, []);
 
+  const deleteItemHandler = (itemId) => {
+    try {
+      fetch(`http://localhost:6630/api/v1/user/${itemId}`, { method: "DELETE", headers: { accept: "*/*" } })
+        .then((response) => response.ok)
+        .then((result) => {
+          if (result) {
+            fetchedDataHandler((prevItems) => prevItems.filter((item) => item.id !== itemId));
+          } else {
+            throw new Error("Something went wrong while deleting the item");
+          }
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       {loadingData ? (
@@ -33,35 +49,44 @@ export default function UsersData(props) {
           Loading Data <TbLoader className={style["loading-icon"]} />
         </p>
       ) : (
-        <ul className={style["ul"]}>
-          {fetchedData.map((item) => {
-            return (
-              <li key={item.id} className={style["item-li"]}>
-                <div className={style["item-container"]}>
-                  <p className={style["item-p"]}>
-                    Name : <span className={style["span"]}>{item.name}</span>
-                  </p>
-                  <p className={style["item-p"]}>
-                    SurName : <span className={style["span"]}>{item.sureName}</span>
-                  </p>
-                  <p className={style["item-p"]}>
-                    Age : <span className={style["span"]}>{item.age}</span>
-                  </p>
-                  <p className={style["item-p"]}>
-                    City : <span className={style["span"]}>{item.city}</span>
-                  </p>
-                  <p className={`${style["item-p"]} ${style["item-message"]}`}>
-                    Message : <span className={style["span"]}>{item.message}</span>
-                  </p>
-                </div>
-                <div className={style["icon-container"]}>
-                  <TbEdit className={style["edit-icon"]} />
-                  <TbArchive className={style["delete-icon"]} />
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+        <div className={style["ul-container"]}>
+          {fetchedData.length !== 0 ? (
+            <ul className={style["ul"]}>
+              {fetchedData.map((item) => {
+                return (
+                  <li key={item.id} className={style["item-li"]}>
+                    <div className={style["item-container"]}>
+                      <p className={style["item-p"]}>
+                        Name : <span className={style["span"]}>{item.name}</span>
+                      </p>
+                      <p className={style["item-p"]}>
+                        SurName : <span className={style["span"]}>{item.sureName}</span>
+                      </p>
+                      <p className={style["item-p"]}>
+                        Age : <span className={style["span"]}>{item.age}</span>
+                      </p>
+                      <p className={style["item-p"]}>
+                        City : <span className={style["span"]}>{item.city}</span>
+                      </p>
+                      <p className={`${style["item-p"]} ${style["item-message"]}`}>
+                        Message : <span className={style["span"]}>{item.message}</span>
+                      </p>
+                    </div>
+                    <div className={style["icon-container"]}>
+                      <TbEdit className={style["edit-icon"]} />
+                      <TbArchive className={style["delete-icon"]} onClick={() => deleteItemHandler(item.id)} />
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            <div className={style["empty-list-container"]}>
+              <TbAlertCircle className={style["nothing-icon"]} />
+              <p className={style["nothing-p"]}>Nothing found!</p>
+            </div>
+          )}
+        </div>
       )}
     </>
   );
