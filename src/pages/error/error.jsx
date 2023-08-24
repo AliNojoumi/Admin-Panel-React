b10a7d67228fa59c7cda6928cc7f90858cecb05b
alinojomi <anojomi3@gmail.com>
2023-08-24 13:20:19 +0330

    Added new text to the error.jsx and made it responsive

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -4,21 +4,25 @@ import { useNavigate } from "react-router-dom";
 export default function Error(props) {
   const navigator = useNavigate();
 
-  const navigateHandler = () => {
-    navigator(-1);
-  };
-
   return (
     <section className={style["error-container"]}>
-      <h1 className={style["h1"]}>Oops! This page is not available!</h1>
+      <h1 className={style["h1"]}>
+        4<span className={style["span"]}>0</span>4!
+      </h1>
       <div className={style["message-container"]}>
+        <h1 className={style["h1-title"]}>Oops! This page was not found!</h1>
         <p className={style["p"]}>
           This page is not available!<br></br> Please return to the previous page by clicking on the button!
         </p>
-        <button className={style["button"]} onClick={navigateHandler}>
-          Go Back
-        </button>
       </div>
+      <button
+        className={style["button"]}
+        onClick={() => {
+          navigator(-1);
+        }}
+      >
+        Go Back
+      </button>
     </section>
   );
 }
